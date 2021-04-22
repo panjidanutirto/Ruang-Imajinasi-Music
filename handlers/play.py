e682be05530d5bb83d521f450ba2ace0cb185f44
Panji Danutirto <51248448+panjidanutirto@users.noreply.github.com>
2021-04-22 20:47:15 +0700

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -24,7 +24,7 @@ async def play(_, message: Message):
     if audio:
         if round(audio.duration / 60) > DURATION_LIMIT:
             raise DurationLimitError(
-                f"**{bn} :-** 😕 Videos longer than {DURATION_LIMIT} minute(s) aren't allowed!\n🤐 The provided video is {audio.duration / 60} minute(s)"
+                f"😕 Audio lebih lama dari {DURATION_LIMIT} menit tidak diperbolehkan!\n🤐 Audio yang disediakan adalah {audio.duration / 60} menit"
             )
 
         file_name = get_file_name(audio)
@@ -35,10 +35,10 @@ async def play(_, message: Message):
     elif url:
         file_path = await converter.convert(youtube.download(url))
     else:
-        return await message.reply_text(f"**{bn} :-** 🙄 You did not give me anything to play!")
+        return await message.reply_text(f"🙄 Anda tidak memberi saya apa pun untuk dimainkan!")
 
     if message.chat.id in callsmusic.pytgcalls.active_calls:
-        await message.reply_text(f"**{bn} :-** 😉 Queued at position #{await callsmusic.queues.put(message.chat.id, file_path=file_path)} !")
+        await message.reply_text(f"😉 Mengantri di posisi #{await callsmusic.queues.put(message.chat.id, file_path=file_path)} !")
     else:
         callsmusic.pytgcalls.join_group_call(message.chat.id, file_path)
-        await message.reply_text(f"**{bn} :-** 🥳 Playing...")
+        await message.reply_text(f"🎶 Memutar...")
